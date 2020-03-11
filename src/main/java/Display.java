bdaae6de8e8dcbc76505043e965e731be6670614
connorgaskell <48499463+connorgaskell@users.noreply.github.com>
2020-03-11 04:23:00 +0000

    Implemented image loading for tiles, middle mouse dragging for camera movement, and other minor changes.

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,5 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Display extends JPanel {
 
@@ -8,13 +12,11 @@ public class Display extends JPanel {
     private int xTiles = 10, yTiles = 10;
     private int selectedTileX = -1, selectedTileY = -1;
 
-    private float xOffset;
-    private float yOffset;
-
-    private float zoom = 1f;
-    private float zoomScale = 10;
+    private float xOffset, yOffset;
+    private float zoom = 1f, zoomScale = 10;
 
     private MouseInput mouseInput;
+    private BufferedImage floorImage, highlightImage;
 
     public Display() {
         mouseInput = new MouseInput(this);
@@ -23,7 +25,16 @@ public class Display extends JPanel {
         addMouseMotionListener(mouseInput);
         addMouseWheelListener(mouseInput);
 
-        setBackground(Color.BLACK);
+        setBackground(Color.darkGray);
+
+        try {
+            floorImage = ImageIO.read(this.getClass().getResource("FloorTile.png"));
+            highlightImage = ImageIO.read(this.getClass().getResource("HighlightTile.png"));
+        } catch (IOException e) { }
+
+        new Timer(1000 / 60, (ActionEvent e) -> {
+            repaint();
+        }).start();
     }
 
     private void createTileMap(Graphics g) {
@@ -33,21 +44,20 @@ public class Display extends JPanel {
         originX = (int)((getWidth() / 2 - xTiles * tileColumnOffset / 2) + xOffset);
         originY = (int)((getHeight() / 2) + yOffset);
 
-        mouseInput.x = mouseInput.x - tileColumnOffset / 2 - originX;
-        mouseInput.y = mouseInput.y - tileRowOffset / 2 - originY;
+        int mouseX = mouseInput.x - tileColumnOffset / 2 - originX;
+        int mouseY = mouseInput.y - tileRowOffset / 2 - originY;
 
-        selectedTileY = (int)Math.round((mouseInput.y + mouseInput.x * 0.5) / tileRowOffset);
-        selectedTileX = -(int)Math.round((mouseInput.y - mouseInput.x * 0.5) / tileRowOffset);
+        selectedTileY = (int)Math.round((mouseY + mouseX * 0.5) / tileRowOffset);
+        selectedTileX = -(int)Math.round((mouseY - mouseX * 0.5) / tileRowOffset);
 
-        System.out.println(selectedTileX + " | " + selectedTileY);
-
-        for(int x = 0; x < xTiles; x++) {
+        for(int x = xTiles - 1; x >= 0; x--) {
             for(int y = 0; y < yTiles; y++) {
                 drawTile(g, x, y, Color.white);
+                drawImage(g, x, y, floorImage);
             }
         }
 
-        if(selectedTileX >= 0 && selectedTileX < xTiles && selectedTileY >= 0 && selectedTileY < yTiles) drawTile(g, selectedTileX, selectedTileY, Color.yellow);
+        if(selectedTileX >= 0 && selectedTileX < xTiles && selectedTileY >= 0 && selectedTileY < yTiles) drawImage(g, selectedTileX, selectedTileY, 4, highlightImage);
     }
 
     private void drawTile(Graphics g, int x, int y, Color color) {
@@ -55,16 +65,21 @@ public class Display extends JPanel {
         int offX = x * tileColumnOffset / 2 + y * tileColumnOffset / 2 + originX;
         int offY = y * tileRowOffset / 2 - x * tileRowOffset / 2 + originY;
 
-        if(x == selectedTileX && y == selectedTileY) {
-            g.setColor(Color.yellow);
-        } else {
-            g.setColor(Color.white);
-        }
-
         g.drawLine(offX, offY + tileRowOffset / 2, offX + tileColumnOffset / 2, offY);
         g.drawLine(offX + tileColumnOffset / 2, offY, offX + tileColumnOffset, offY + tileRowOffset / 2);
         g.drawLine(offX + tileColumnOffset, offY + tileRowOffset / 2, offX + tileColumnOffset / 2, offY + tileRowOffset);
         g.drawLine(offX + tileColumnOffset / 2, offY + tileRowOffset, offX, offY + tileRowOffset / 2);
+    }
+
+    private void drawImage(Graphics g, int x, int y, BufferedImage image) {
+        drawImage(g, x, y, 0, image);
+    }
+
+    private void drawImage(Graphics g, int x, int y, int heightOffset, BufferedImage image) {
+        int offX = x * tileColumnOffset / 2 + y * tileColumnOffset / 2 + originX;
+        int offY = y * tileRowOffset / 2 - x * tileRowOffset / 2 + originY;
+
+        g.drawImage(image, offX, offY - (int) ((28 + heightOffset) * zoom), (int) (image.getWidth() * zoom), (int) (image.getHeight() * zoom), null);
     }
 
     public void setZoom(float value) {
@@ -73,13 +88,23 @@ public class Display extends JPanel {
     }
 
     public void setOffset(float x, float y) {
-        xOffset = xOffset + x;
-        yOffset = yOffset + y;
+        setOffset(x, y, 250);
+    }
+
+    public void setOffset(float x, float y, float max) {
+        max = max * zoom;
+        xOffset = xOffset >= max && x > 0 ? max : xOffset <= -max && x < 0 ? -max : xOffset + x;
+        yOffset = yOffset >= max && y > 0 ? max : yOffset <= -max && y < 0 ? -max : yOffset + y;
+    }
+
+    public void dragOffset(float distanceX, float distanceY, float dragSpeed) {
+        setOffset(distanceX * dragSpeed, distanceY * dragSpeed);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         createTileMap(g);
+        dragOffset(mouseInput.dragDistanceX, mouseInput.dragDistanceY, 2.0f);
     }
 
 }
--- src/main/java/MouseInput.java
+++ src/main/java/MouseInput.java
@@ -6,6 +6,10 @@ public class MouseInput implements MouseListener, MouseWheelListener, MouseMotio
     public int x;
     public int y;
 
+    private int dragStartX, dragStartY;
+    public int dragDistanceX, dragDistanceY;
+    private boolean middleMouseDown = false;
+
     public MouseInput(Display display) {
         this.display = display;
     }
@@ -17,12 +21,20 @@ public class MouseInput implements MouseListener, MouseWheelListener, MouseMotio
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        if(e.getButton() == MouseEvent.BUTTON2) {
+            middleMouseDown = true;
+            dragStartX = e.getX();
+            dragStartY = e.getY();
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        if(e.getButton() == MouseEvent.BUTTON2) {
+            middleMouseDown = false;
+            dragDistanceX = 0;
+            dragDistanceY = 0;
+        }
     }
 
     @Override
@@ -38,18 +50,19 @@ public class MouseInput implements MouseListener, MouseWheelListener, MouseMotio
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
         display.setZoom(-e.getWheelRotation());
-        display.repaint();
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-
+        if(middleMouseDown) {
+            dragDistanceX = (dragStartX - e.getX()) / 50;
+            dragDistanceY = (dragStartY - e.getY()) / 50;
+        }
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
         x = e.getX();
         y = e.getY();
-        display.repaint();
     }
 }
